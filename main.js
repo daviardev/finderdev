71494322c78068d209dbc29ff4fd7ae3de81e98d
daviardev <jerson.silva99@hotmail.com>
2023-06-15 01:11:47 -0500

    Capture event and validate fields

--- main.js
+++ main.js
@@ -3,3 +3,103 @@ import './style.css'
 import { $ } from './src/lib/dom'
 
 $('#app')
+
+// Listen submit event
+const form = $('form')
+const input = $('#username')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  // Get username input
+  const username = input.value.trim()
+
+  if (!username) return
+
+  input.value = ''
+  getUser(username)
+})
+
+// Query GitHun Api username
+const getUser = async username => {
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}`)
+
+    if (!res.ok) throw new Error('User dont found')
+    const data = await res.json()
+    showUserData(data)
+  } catch (err) {
+    console.error(err)
+    return null
+  }
+}
+
+const showUserData = data => {
+  const userData = `
+    <article class='grid-areas card-info'>
+    <div class='section-logo logo-content'>
+      <img
+        src='https://githubsearchusersdevfinder.vercel.app/_next/image?url=%2F65743790.png&w=96&q=75'
+        alt='Profile url'
+        loading='lazy'
+        decoding='async'
+        data-nimg='1'
+      >
+    </div>
+    <div class='section-title'>
+      <h1>daviardev</h1>
+      <p>@jersonsilvaa</p>
+    </div>
+    <p class='section-date date'>
+      21 de mayo de 2020
+    </p>
+    <p class='section-description description'>
+      Training Developer Web App, Game Developer, Mobile Developer
+    </p>
+    <div class='section-number info'>
+      <article>
+        <p>Repos</p>
+        <p class='repos'>51</p>
+      </article>
+      <article>
+        <p>Followers</p>
+        <p class='repos'>3</p>
+      </article>
+      <article>
+        <p>Following</p>
+        <p class='repos'>1</p>
+      </article>
+    </div>
+    <div class='section-social social'>
+      <article class='position-elements'>
+        <i>
+          <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 24 24' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'><path d='M12 14c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2z'></path><path d='M11.42 21.814a.998.998 0 0 0 1.16 0C12.884 21.599 20.029 16.44 20 10c0-4.411-3.589-8-8-8S4 5.589 4 9.995c-.029 6.445 7.116 11.604 7.42 11.819zM12 4c3.309 0 6 2.691 6 6.005.021 4.438-4.388 8.423-6 9.73-1.611-1.308-6.021-5.294-6-9.735 0-3.309 2.691-6 6-6z'></path></svg>
+        </i>
+        <span class='position'>Colombia</span>
+      </article>
+
+      <article class='position-elements'>
+        <i>
+          <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 24 24' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'><path d='M8.465 11.293c1.133-1.133 3.109-1.133 4.242 0l.707.707 1.414-1.414-.707-.707c-.943-.944-2.199-1.465-3.535-1.465s-2.592.521-3.535 1.465L4.929 12a5.008 5.008 0 0 0 0 7.071 4.983 4.983 0 0 0 3.535 1.462A4.982 4.982 0 0 0 12 19.071l.707-.707-1.414-1.414-.707.707a3.007 3.007 0 0 1-4.243 0 3.005 3.005 0 0 1 0-4.243l2.122-2.121z'></path><path d='m12 4.929-.707.707 1.414 1.414.707-.707a3.007 3.007 0 0 1 4.243 0 3.005 3.005 0 0 1 0 4.243l-2.122 2.121c-1.133 1.133-3.109 1.133-4.242 0L10.586 12l-1.414 1.414.707.707c.943.944 2.199 1.465 3.535 1.465s2.592-.521 3.535-1.465L19.071 12a5.008 5.008 0 0 0 0-7.071 5.006 5.006 0 0 0-7.071 0z'></path></svg>
+        </i>
+        <a class='position' href='https://daviar.dev.vercel.app/'>daviar.dev.vercel.app/</a>
+      </article>
+
+      <article class='position-elements'>
+        <i>
+          <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 16 16' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'><path d='M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z'></path></svg>
+        </i>
+        <a class='position' href='https://www.twitter.com/bluuweb'>daviardev</a>
+      </article>
+
+      <article class='position-elements'>
+        <i>
+          <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 16 16' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'><path d='M3 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h3v-3.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V16h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1H3Zm1 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5Z'></path>
+          </svg>
+        </i>
+        <span class='position'>Google</span>
+      </article>
+    </div>
+  </article>
+`
+}
